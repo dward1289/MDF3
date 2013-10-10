b4019105b2bb0572f83431a55e0982e24bf37544
devona <dward1289@fullsail.edu>
2013-10-09 23:22:15 -0400

    WriteRead file added. Capture photo displays

--- Week2/PictureThis/src/com/DevonaWard/picturethis/MainActivity.java
+++ Week2/PictureThis/src/com/DevonaWard/picturethis/MainActivity.java
@@ -1,48 +1,42 @@
 package com.DevonaWard.picturethis;
 
-import android.net.Uri;
+
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.view.Menu;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class MainActivity extends Activity {
 
-	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-	private Uri fileUri;
+	private static final int PHOTO_CODE = 100;
+
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		//Camera intent: take picture
-		Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		
-		//File name for image here:
-		//fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
-		cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-		
-		//Starts camera
-		startActivityForResult(cameraIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+	}
+
+	//Capture photo
+	public void capturePicture(View view){
+		Intent cameraIntent=new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(cameraIntent, PHOTO_CODE);
 	}
 	
-	
+
+	//Displays photo after capture has been completed
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-	        if (resultCode == RESULT_OK) {
-	            // Image captured and saved to fileUri specified in the Intent
-	            Toast.makeText(this, "Image saved to:\n" +
-	                     data.getData(), Toast.LENGTH_LONG).show();
-	        } else if (resultCode == RESULT_CANCELED) {
-	            // User cancelled the image capture
-	        } else {
-	            // Image capture failed, advise user
-	        }
-	    }
+		if (requestCode == PHOTO_CODE) { 
+            Bitmap photo = (Bitmap) data.getExtras().get("data");            
+            ImageView iv = (ImageView)findViewById(R.id.imageView);
+            iv.setImageBitmap(photo);
+ }
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
