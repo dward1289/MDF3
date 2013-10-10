23e8186ca70a654eb7509a82343891c004db98a0
devona <dward1289@fullsail.edu>
2013-10-10 01:35:01 -0400

    Vibration,and sound added to notification. App now refreshes. Photos save to gallery.

--- Week2/PictureThis/src/com/DevonaWard/picturethis/MainActivity.java
+++ Week2/PictureThis/src/com/DevonaWard/picturethis/MainActivity.java
@@ -1,7 +1,21 @@
+/*
+ * Project	PictureThis
+ * 
+ * Package	com.DevonaWard.picturethis
+ * 
+ * @author Devona Ward
+ * 
+ * Date: Oct 10, 2013
+ */
 package com.DevonaWard.picturethis;
 
 
+
+
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -10,6 +24,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 
@@ -17,17 +32,14 @@ public class MainActivity extends Activity {
 
 	private static final int PHOTO_CODE = 100;
 	
-
+	Bitmap photo;
+	String pictureName;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		
-		 
-		
-		
+		setContentView(R.layout.activity_main);	
 	}
 
 	//Capture photo
@@ -41,7 +53,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == PHOTO_CODE) { 
-            Bitmap photo = (Bitmap) data.getExtras().get("data");            
+            photo = (Bitmap) data.getExtras().get("data");            
             ImageView iv = (ImageView)findViewById(R.id.imageView);
             iv.setImageBitmap(photo);     
 		}
@@ -49,17 +61,30 @@ public class MainActivity extends Activity {
 	
 	//Saves photo and photo name, displays notification to notify user of the saved photo
 	public void saveButton(View view){
+		
+		EditText picTxt = (EditText)findViewById(R.id.picName);
+		pictureName = picTxt.getText().toString();
+		
+		MediaStore.Images.Media.insertImage(getContentResolver(), photo, pictureName , "PictureThis");
+
 		//prepare intent which is triggered if the notification is selected
 		Intent intent = new Intent(this, Notification.class);
 		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
+		//Notification sound
+		Uri theSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+		
+		//Vibration pattern found at: http://stackoverflow.com/questions/12114917/notification-setfullscreenactivity
+		long[] vibration = { 0, 500, 250, 500 };
 		// build notification
 		Notification notification  = new Notification.Builder(this)
+		
 		        .setContentTitle("PictureThis")
-		        .setContentText("Your photo has been saved.")
+		        .setContentText("Your photo has "+pictureName+" been saved.")
 		        .setSmallIcon(R.drawable.ic_launcher)
 		        .setContentIntent(pendingIntent)
-		        .setWhen(0)
+		        .setSound(theSound)
+		        .setVibrate(vibration)
 		        .setAutoCancel(true)
 		        .build();
 		    
@@ -68,6 +93,10 @@ public class MainActivity extends Activity {
 
 		//Display notification
 		notificationManager.notify(0, notification);
+		
+		//Refreshes app
+		finish();
+		startActivity(getIntent());
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
