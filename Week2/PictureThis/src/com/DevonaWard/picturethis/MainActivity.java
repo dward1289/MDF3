0931f64c9bbac32e69547cbea069009099817a69
devona <dward1289@fullsail.edu>
2013-10-10 00:53:35 -0400

    Notification added, save button added, edit text field for picture name added. Notification works.

--- Week2/PictureThis/src/com/DevonaWard/picturethis/MainActivity.java
+++ Week2/PictureThis/src/com/DevonaWard/picturethis/MainActivity.java
@@ -3,6 +3,9 @@ package com.DevonaWard.picturethis;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.view.Menu;
@@ -13,13 +16,18 @@ import android.widget.ImageView;
 public class MainActivity extends Activity {
 
 	private static final int PHOTO_CODE = 100;
-
+	
 
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		 
+		
+		
 	}
 
 	//Capture photo
@@ -35,8 +43,31 @@ public class MainActivity extends Activity {
 		if (requestCode == PHOTO_CODE) { 
             Bitmap photo = (Bitmap) data.getExtras().get("data");            
             ImageView iv = (ImageView)findViewById(R.id.imageView);
-            iv.setImageBitmap(photo);
- }
+            iv.setImageBitmap(photo);     
+		}
+	}
+	
+	//Saves photo and photo name, displays notification to notify user of the saved photo
+	public void saveButton(View view){
+		//prepare intent which is triggered if the notification is selected
+		Intent intent = new Intent(this, Notification.class);
+		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+		// build notification
+		Notification notification  = new Notification.Builder(this)
+		        .setContentTitle("PictureThis")
+		        .setContentText("Your photo has been saved.")
+		        .setSmallIcon(R.drawable.ic_launcher)
+		        .setContentIntent(pendingIntent)
+		        .setWhen(0)
+		        .setAutoCancel(true)
+		        .build();
+		    
+		  
+		NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+		//Display notification
+		notificationManager.notify(0, notification);
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
